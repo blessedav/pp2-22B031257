d1675b6502304c4a61759b5c309866cdf238fa23
blessedav <gimisi@bk.ru>
2023-02-10 09:13:03 +0600

    6.py

---
+++ tsis 1/6.py
@@ -0,0 +1,27 @@
+x = 5
+print(type(x))
+#it will print "int"
+
+x = "Hello world"
+print(type(x))
+#it will print "str"
+
+x = 20.5
+print(type(x))
+#it will print "float"
+
+x = ["apple", "banana" , "cherry"]
+print(type(x))
+#it will print "list"
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+#it will print "tuple"
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#it will print "dict"
+
+x = True
+print(type(x))
+#it will print "bool"
