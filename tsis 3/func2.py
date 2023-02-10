0ae6711946b1ef1c8182bf0cfced638f51ee7c3b
blessedav <gimisi@bk.ru>
2023-02-10 13:43:37 +0600

    classes.py

--- tsis 3/func2.py
+++ tsis 3/func2.py
@@ -125,9 +125,3 @@ cat = input()
 movs = []
 avg = 0
 imdbCatAvg()
-
-example_list = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]
-
-prime_list = list(filter(lambda x: all(x%i != 0 for i in range(2 , int(x)**0.5+1)) and x!= 1 ,example_list))
-
-print(prime_list)
