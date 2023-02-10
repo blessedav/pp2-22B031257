72f7f34d74adb406799a8e54405c4ced43787103
blessedav <gimisi@bk.ru>
2023-02-10 09:28:40 +0600

    7.py

---
+++ tsis 1/11.py
@@ -0,0 +1,14 @@
+print(10>9)
+#it will print "True"
+
+print(10 == 9)
+#it will print "False"
+
+print(10 < 9)
+#it wll print "False"
+
+print(bool("abc"))
+#it will print "True"
+
+print(bool(0))
+#it will print "False"
---
+++ tsis 1/9.py
@@ -0,0 +1,24 @@
+x = "Hello world"
+print(len(x))
+
+txt = "Hello world"
+x = txt[0]
+
+txt = "Hello world"
+x = txt[2:5]
+
+txt = " Hello world "
+x = txt.strip()
+
+txt = "Hello World"
+txt = txt.upper()
+
+txt = "Hello world"
+txt = txt.lower()
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
