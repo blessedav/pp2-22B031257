7a23151c7a789ea5d9c5f1b73f074066f7e65ffc
blessedav <gimisi@bk.ru>
2023-02-27 03:34:03 +0600

    tsis 4

---
+++ tsis 4/date/2.py
@@ -0,0 +1,6 @@
+import datetime
+from datetime import datetime, time, timedelta, date
+dt = date.today()
+dy = date.today() - timedelta(1)
+dt2 = date.today() + timedelta(1)
+print(dy, dt, dt2)
---
+++ tsis 4/date/3.py
@@ -0,0 +1,3 @@
+import datetime
+from datetime import datetime, time, timedelta, date
+print(datetime.datetime.today().replace(microsecond = 0))
